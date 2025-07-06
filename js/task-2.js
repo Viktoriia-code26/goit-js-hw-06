421cfe5565dfa89d1aa499eb75b3216144b98995
Viktoriia P. <pohrebniakvika@gmail.com>
2025-07-06 07:32:55 +0200

    js/task-2.js inclyded

--- js/task-2.js
+++ js/task-2.js
@@ -22,7 +22,7 @@ class Storage{
     removeItem(itemToRemove) {
         const index = this.#items.indexOf(itemToRemove);
         if (index !== -1) {
-            this.items.splice(index, 1);
+            this.#items.splice(index, 1);
         }
     }
 }
